a0c5e8673802651a0668d742885860b7fbf6da15
Elianagam <elianagam2@gmail.com>
2022-10-10 17:22:51 -0300

    Add files

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = 3000;
 const TIMEOUT = 5 * 1000;
+const url_sync = 'http://bbox:9090/'
+const url_async = 'http://bbox:9091/'
 
 app.get('/', (req, res) => {
   res.status(200).send('ping');
@@ -16,3 +18,17 @@ app.get('/heavy', (req, res) => {
 app.listen(PORT, function () {
   console.log('App listening on port', PORT);
 });
+
+app.get('/sync', (req, res) => {
+    //metrics('sync')
+    axios.get(url_sync).then(res_service => {
+        res.status(200).send(`Res sync service\n ${res_service.data}\n`)
+    })
+})
+
+app.get('/async', (req, res) => {
+    //metrics('async')
+    axios.get(url_async).then(res_service => {
+        res.status(200).send(`Res async service\n ${res_service.data}\n`)
+    })
+})
