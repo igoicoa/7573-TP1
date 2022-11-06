b651ea0aef2db2bacea90e9e12b211c0a81ccfcb
spenna <sebapenna19@gmail.com>
2022-11-06 18:53:09 -0300

    Fix statsd configuration

--- app/main.js
+++ app/main.js
@@ -1,20 +1,23 @@
+const axios = require('axios');
 const express = require('express');
-var moment = require('moment')
+const moment = require('moment');
 const app = express();
 
 const PORT = 3000;
 const TIMEOUT = 5 * 1000;
-const url_sync = 'http://bbox:9090/'
-const url_async = 'http://bbox:9091/'
+const url_sync = 'http://bbox:9090/';
+const url_async = 'http://bbox:9091/';
 
 const StatsD = require('node-statsd'),
-      client = new StatsD();
+      client = new StatsD(host = "graphite", port = 8125);
 
 // Increment: Increments a stat by a value (default is 1)
-function increment_metric(time, metric_name) {
+function increment_metric(firstTime, metric_name) {
+  const secondTime = moment();
+  const timeDifference = secondTime.diff(firstTime, 'millisecond');
+  console.log(metric_name + ": " + timeDifference);
   client.increment('request_count_' + metric_name);
-  client.gauge('request_time_' + metric_name, time);
-
+  client.gauge('request_time_' + metric_name, timeDifference);
 }
 
 
@@ -22,49 +25,37 @@ function increment_metric(time, metric_name) {
 // Gauge: Gauge a stat by a specified amount
 
 app.get('/', (req, res) => {
-  var firstTime = moment();
+  const firstTime = moment();
   
   res.status(200).send('ping');
-
-  var secondTime = moment();
-  var timeDifference = secondTime.diff(firstTime, 'seconds')
-  increment_metric(timeDifference, 'ping')
+  increment_metric(firstTime, 'ping');
 });
 
 app.get('/heavy', (req, res) => {
-  var firstTime = moment();
+  const firstTime = moment();
   for (t = new Date(); new Date() - t < TIMEOUT; );
   res.status(200).send('heavy');
-
-  var secondTime = moment();
-  var timeDifference = secondTime.diff(firstTime, 'seconds')
-  increment_metric(timeDifference, 'heavy')
+  increment_metric(firstTime, 'heavy');
 });
 
 app.listen(PORT, function () {
   console.log('App listening on port', PORT);
 });
 
-app.get('/sync-proxy', (req, res) => {
-  var firstTime = moment();
+  app.get('/sync-proxy', (req, res) => {
+  const firstTime = moment();
 
   axios.get(url_sync).then(res_service => {
-    res.status(200).send('Sync')
-  })
-
-  var secondTime = moment();
-  var timeDifference = secondTime.diff(firstTime, 'seconds')
-  increment_metric(timeDifference, 'sync')
-})
+    res.status(200).send(res_service.data);
+    increment_metric(firstTime, 'sync');
+  });
+});
 
 app.get('/async-proxy', (req, res) => {
-  var firstTime = moment();
+  const firstTime = moment();
 
   axios.get(url_async).then(res_service => {
-    res.status(200).send('Async')
-  })
-
-  var secondTime = moment();
-  var timeDifference = secondTime.diff(firstTime, 'seconds')
-  increment_metric(timeDifference, 'async')
-})
+    res.status(200).send(res_service.data);
+    increment_metric(firstTime, 'async');
+  });
+});
